785659368eea02a7a289aa7dd10cd0d82bd5cc55
gaurigattani <gaurisoft@gmail.com>
2015-03-13 19:51:22 +0530

    application structure
    All the basic files required to get App up and running

---
+++ app/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('DuckDuckGoSearchApp', ['ngRoute', 'ngResource', 'ui.bootstrap']);
+
+/* configure the routes. */
+app.config(function ($routeProvider) {
+ 
+/*  We only have two routes as of now -> Search and About */
+    $routeProvider.when("/search", {
+        controller: "searchResultsController",
+        templateUrl: "app/views/searchresults.html"
+    });		
+	
+	$routeProvider.when("/about", {
+       templateUrl: "app/views/about.html"
+    });
+	
+	/* return to URL /search when ever a matching URL is not found */
+    $routeProvider.otherwise({ redirectTo: "/search" });
+ 
+});
